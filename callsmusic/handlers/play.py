e1c4155331552c6372e48664bdace9b7e2ed968e
kyiushinz <84819080+kyiushinz@users.noreply.github.com>
2021-07-04 21:46:41 +0700

    fixed

--- callsmusic/handlers/play.py
+++ callsmusic/handlers/play.py
@@ -22,7 +22,7 @@ async def play(_, message: Message):
     audio = (
         message.reply_to_message.audio or message.reply_to_message.voice
     ) if message.reply_to_message else None
-    response = await message.reply_text('<b>🔄 Processing...</b>', False)
+    response = await message.reply_text('<b>🔄 Sabar ya anjing 👉😁👈...</b>', False)
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
@@ -74,7 +74,7 @@ async def play(_, message: Message):
             url = text_links[0].url
         else:
             await response.edit_text(
-                '<b>❌ You did not give me anything to play</b>',
+                '<b>❌ Mana lagu/song download nya anjing 👉😁👈..</b>',
             )
             return
 
@@ -83,8 +83,8 @@ async def play(_, message: Message):
     if chat_id in callsmusic.active_chats:
         position = await queues.put(chat_id, file=file)
         await response.edit_text(
-            f'<b>#️⃣ Queued at position {position}</b>...',
+            f'<b>#️⃣ Music dalam antrian anjing 👉😁👈... {position}</b>...',
         )
     else:
         await callsmusic.set_stream(chat_id, file)
-        await response.edit_text('<b>▶️ Playing...</b>')
+        await response.edit_text('<b>▶️ Lagu di mulai anjing 👉😁👈...</b>')
